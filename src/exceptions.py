f81173c86bc83eaf027a3ed9bd218c6a15b94c88
Danil Fedorov <neweternalicon@gmail.com>
2026-02-24 21:47:30 +0300

    removed exceptions

--- src/exceptions.py
+++
@@ -1,8 +0,0 @@
-class BaseServiceError(Exception):
-    def __init__(self, *, message: str | None = None, **context) -> None:
-        self.message = message or self.__doc__
-        self.context = context
-
-
-class PathDoesNotExist(BaseServiceError):
-    """Path does not exist."""
